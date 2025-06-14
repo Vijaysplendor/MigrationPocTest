95c44bb21e9cf3274e23043ad3049b0f81a14ddc
Vijaysplendor <vijaysplendor@gmail.com>
2025-06-14 16:18:37 +0200

    Update migaccpushfile.py

--- migaccpushfile.py
+++ migaccpushfile.py
@@ -1,17 +1,17 @@
 import base64
+import os
 import requests
 from requests.auth import HTTPBasicAuth
 import json
-import os
 
 # Azure DevOps organization URL and PAT
 pat = os.getenv("MY_SECRET_KEY")
 repository = "PartsUnlimited"
-authorization = str(base64.b64encode(bytes(':'+pat, 'ascii')), 'ascii')
-headers = {
-         'Accept': 'application/json',
-         'Authorization': 'Basic '+authorization
-    }
+#authorization = str(base64.b64encode(bytes(':'+pat, 'ascii')), 'ascii')
+#headers = {
+#         'Accept': 'application/json',
+#         'Authorization': 'Basic '+authorization
+#    }
 
 #Rest API End Points
 initial_base_url = f"https://dev.azure.com/VI20040428/PartsUnlimitedTest/_apis/git/repositories/{repository}"
@@ -19,7 +19,7 @@ repo_id_url = f"{initial_base_url}?api-version=6.0"
 
 # code to get the repository_id
 def get_repo_id():
-    response = requests.get(repo_id_url, headers=headers)
+    response = requests.get(repo_id_url, auth=HTTPBasicAuth("", pat))
     if response.status_code == 200:
         #Parse the JSON response
         repo_data = response.json()
